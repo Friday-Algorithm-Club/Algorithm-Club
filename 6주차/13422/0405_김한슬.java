3be70e1fa9691642756545cdb1891d5a01e28a57
slcloe <67732600+slcloe@users.noreply.github.com>
2024-10-14 22:35:29 +0900

    6주차 도둑 solved

--- 6주차/13422/0405_김한슬.java
+++ 6주차/13422/0405_김한슬.java
@@ -1,8 +1,56 @@
-public class week_13422_0406_김한슬 {
-    /*
-    누적합 + 투포인터
-    1. 1 ~ M 까지의 합을 구함
-    2. K와 비교하여 K보다 작은지 확인
-    3. i - M번째의 수를 뺀 후 M + 1을 더하면 항상 M개의 합에 대해 비교할 수 있다. 2번으로 돌아가 합과 K와의 비교를 진행한다.
-     */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int T, N, M, K;
+    static int[] arr;
+    static int[] sum;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        T = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            arr = new int[N];
+
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            sum = new int[N + M];
+
+            for (int j = 0; j < N + M - 1; j++) {
+                sum[j + 1] = sum[j] + arr[j % N];
+            }
+
+            int cnt = 0;
+            int size = N;
+            if (N == M)
+                size = 1;
+            for (int j = 0; j < size; j++) {
+                if (sum[j + M] - sum[j] < K) cnt++;
+            }
+            sb.append(cnt).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
 }
+
+/*
+1
+5 5 16
+1 2 3 4 5
+
+ */
